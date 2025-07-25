aae4cee6cbff71e4e9de2f9bc358fede886d8207
Fredrick Kebaso <fredrickkebaso@gmail.com>
2025-07-25 19:28:45 +0300

    Update Tools_install.sh

--- Installation_scripts/Tools_install.sh
+++ Installation_scripts/Tools_install.sh
@@ -18,39 +18,3 @@ for tool in ${tools[@]}; do
     echo "${tool} installed successfully"
 
 done
-
-echo checking all tools installed successfully!!!
-
-#!/bin/bash
-
-# Tool -> Command mapping
-declare -A tools_cmds=(
-  [fastqc]="fastqc"
-  [multiqc]="multiqc"
-  [trim-galore]="trim_galore"
-  [kmergenie]="kmergenie"
-  [jellyfish]="jellyfish"
-  [spades]="spades.py"
-  [velvet]="velveth"
-  [bwa]="bwa"
-  [seqkit]="seqkit"
-  [samtools]="samtools"
-  [pilon]="pilon"
-  [quast]="quast"
-  [busco]="busco"
-)
-
-# Activate conda shell
-eval "$(conda shell.bash hook)"
-
-for tool in "${!tools_cmds[@]}"; do
-  conda activate "$tool" &> /dev/null
-  
-  if command -v "${tools_cmds[$tool]}" &> /dev/null; then
-    echo "✅ $tool"
-  else
-    echo "❌ $tool"
-  fi
-
-  conda deactivate
-done
