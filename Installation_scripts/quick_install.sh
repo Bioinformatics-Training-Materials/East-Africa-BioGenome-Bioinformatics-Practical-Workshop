974fcdd8f2e1da6d2aebfd8e63c25f4172cf5a76
Fredrick Kebaso <fredrickkebaso@gmail.com>
2025-07-25 19:26:31 +0300

    tools_install.sh

--- Installation_scripts/quick_install.sh
+++ Installation_scripts/quick_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script sets up a conda environment for all the tools needed for the project.
+# This script sets up a conda environment for all the tools and installs the tools needed for the project.
 # It assumes that conda is already installed and available in the PATH. 
 
 tools=(fastqc multiqc trim-galore kmergenie jellyfish spades velvet seqkit bwa samtools pilon quast busco)
