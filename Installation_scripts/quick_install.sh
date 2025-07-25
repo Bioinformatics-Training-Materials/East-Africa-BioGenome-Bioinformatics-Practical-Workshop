afc40ab72a76f1cfad1cc78cf388696ecd13cb19
Fredrick Kebaso <fredrickkebaso@gmail.com>
2025-07-25 17:52:44 +0300

    Update quick_install.sh

--- Installation_scripts/quick_install.sh
+++ Installation_scripts/quick_install.sh
@@ -11,6 +11,10 @@ bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
 # Initialize Conda for current shell
 $HOME/miniconda3/bin/conda init
 
+# Accept ToS for required Anaconda channels
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
 # Activate changes
 source ~/.bashrc
 
